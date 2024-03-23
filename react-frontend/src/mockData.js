76d595eb380a890ed383033099e480ae43e7eacd
zachmanac <zachstephenson94@gmail.com>
2024-03-23 12:03:50 -0700

    add mockdata, install sass, styling, linting

---
+++ react-frontend/src/mockData.js
@@ -0,0 +1,34 @@
+const data = { "data": [
+  {
+      "assists": 81,
+      "evGoals": 29,
+      "evPoints": 77,
+      "faceoffWinPct": 0.50000,
+      "gameWinningGoals": 6,
+      "gamesPlayed": 68,
+      "goals": 41,
+      "lastName": "Kucherov",
+      "otGoals": 1,
+      "penaltyMinutes": 22,
+      "playerId": 8476453,
+      "plusMinus": 6,
+      "points": 122,
+      "pointsPerGame": 1.79411,
+      "positionCode": "R",
+      "ppGoals": 12,
+      "ppPoints": 45,
+      "seasonId": 20232024,
+      "shGoals": 0,
+      "shPoints": 0,
+      "shootingPct": 0.15413,
+      "shootsCatches": "L",
+      "shots": 266,
+      "skaterFullName": "Nikita Kucherov",
+      "teamAbbrevs": "TBL",
+      "timeOnIcePerGame": 1303.5882
+    }
+  ],
+  "total": 1
+}
+
+export default data;
