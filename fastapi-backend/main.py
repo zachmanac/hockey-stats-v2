83c89f8d28c5e6786e39c0ea2ef36e07680f4e25
zachmanac <zachstephenson94@gmail.com>
2024-03-25 11:46:48 -0700

    add route for fetching nhl player data

--- fastapi-backend/main.py
+++ fastapi-backend/main.py
@@ -1,3 +1,4 @@
+import requests
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -14,3 +15,23 @@ app.add_middleware(
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/player/{player_name}")
+async def read_player_stats(player_name: str):
+    url = "https://api.nhle.com/stats/rest/en/skater/summary"
+    params = {
+        "isAggregate": "false",
+        "isGame": "false",
+        "sort": '[{"property":"points","direction":"DESC"},{"property":"goals","direction":"DESC"},{"property":"assists","direction":"DESC"},{"property":"playerId","direction":"ASC"}]',
+        "start": 0,
+        "limit": 50,
+        "factCayenneExp": "gamesPlayed>=1",
+        "cayenneExp": "gameTypeId=2 and seasonId<=20232024 and seasonId>=20232024 and skaterFullName likeIgnoreCase '%" + player_name + "%'"
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return {"error": "Unable to fetch player"}
