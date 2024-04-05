2b1dd058fa37b6aea015f2bcddf2de23ff7687eb
zachmanac <zachstephenson94@gmail.com>
2024-04-04 20:34:57 -0700

    add range loop for fetching all seasons of data to train model on

--- fastapi-backend/dataframe.py
+++ fastapi-backend/dataframe.py
@@ -1,40 +1,40 @@
-import json
 import pandas as pd
-from pandas import json_normalize
-
 from supabase import create_client, Client
 
 url: str = "https://gwqnevtnmqmlqwgyvtqb.supabase.co"
 key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd3cW5ldnRubXFtbHF3Z3l2dHFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEzMzgxNzksImV4cCI6MjAyNjkxNDE3OX0.4rcu3MU6w258JYbjcX2PvtIcVd8xIAmOekJoDeyBYZQ"
 
-# filtered_df = df[df['season'] >= 5]
-
 def create_dataframe(supabase_url, supabase_key):
 
   supabase: Client = create_client(supabase_url, supabase_key)
+  merged_dfs = []
 
-  player_stats = supabase.table('player_stats') \
-    .select('*') \
-    .eq('season_id', 20212022) \
-    .order('points', desc=True) \
-    .limit(1005) \
-    .execute()
+  for season_id in range(20052006, 20222023, 10001):
 
-  df = pd.DataFrame(player_stats.data)
+# default limit is 1000, limit must be 1004 for 20212022
+    player_stats = supabase.table('player_stats') \
+      .select('*') \
+      .eq('season_id', season_id) \
+      .order('points', desc=True) \
+      .limit(1005) \
+      .execute()
 
-  players = supabase.table('players') \
-    .select('*') \
-    .limit(4000) \
-    .order('name', desc=True) \
-    .execute()
-  
-  df1 = pd.DataFrame(players.data)
+    df = pd.DataFrame(player_stats.data)
 
-  # Merge the player_stats DataFrame with the players DataFrame
-  merged_df = pd.merge(df, df1, on='player_id', how='inner')
-  
-  return merged_df
-  
+    players = supabase.table('players') \
+      .select('*') \
+      .limit(4000) \
+      .execute()
+    
+    df1 = pd.DataFrame(players.data)
+
+    # Merge the player_stats DataFrame with the players DataFrame
+    merged_df = pd.merge(df, df1, on='player_id', how='inner')
+    merged_dfs.append(merged_df)
+    
+  final_df = pd.concat(merged_dfs, ignore_index=True)
+  return final_df
+    
 df = create_dataframe(url, key)
 
 print(df)
