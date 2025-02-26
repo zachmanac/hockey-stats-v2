863bad6f7c5c9bbb7c5a0b6ab68e91c3636749fe
zachmanac <zachstephenson94@gmail.com>
2025-02-25 21:38:23 -0800

    change supabase project key/string

--- fastapi-backend/fetch_player_data.py
+++ fastapi-backend/fetch_player_data.py
@@ -1,8 +1,8 @@
 from supabase import create_client, Client
 import requests
 
-url: str = "https://gwqnevtnmqmlqwgyvtqb.supabase.co"
-key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd3cW5ldnRubXFtbHF3Z3l2dHFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEzMzgxNzksImV4cCI6MjAyNjkxNDE3OX0.4rcu3MU6w258JYbjcX2PvtIcVd8xIAmOekJoDeyBYZQ"
+url: str = "https://cserpqafduzvhzigjtwa.supabase.co"
+key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNzZXJwcWFmZHV6dmh6aWdqdHdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1NDgxODUsImV4cCI6MjA1NjEyNDE4NX0.sKjF8KmoymgWVnisaHRDxKesibIZHReNTyVXdThY6p0"
 supabase: Client = create_client(url, key)
 
 # range(0, max amount of player in hundreds, increment step(should be 100))
@@ -12,7 +12,7 @@ def fetch_player_data():
     player_data = []
     season_id = 20232024
 
-    for n in range(0, 900, 100):
+    for n in range(0, 1100, 100):
         url = f"https://api.nhle.com/stats/rest/en/skater/summary?isAggregate=false&isGame=false&sort=%5B%7B%22property%22:%22points%22,%22direction%22:%22DESC%22%7D,%7B%22property%22:%22goals%22,%22direction%22:%22DESC%22%7D,%7B%22property%22:%22assists%22,%22direction%22:%22DESC%22%7D,%7B%22property%22:%22playerId%22,%22direction%22:%22ASC%22%7D%5D&start={n}&limit=100&factCayenneExp=gamesPlayed%3E=1&cayenneExp=gameTypeId=2%20and%20seasonId%3C={season_id}%20and%20seasonId%3E={season_id}"
         response = requests.get(url)
         if response.status_code == 200:
